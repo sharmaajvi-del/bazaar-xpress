ac9a4ed24f7d3c108a9fbcf79101829d6deacda8
sharmaajvi-del <sharmaajvi@gmail.com>
2026-06-29 06:59:21 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,39 @@
+const products = [
 
+{
+id:1,
+name:"iPhone 15",
+price:69999,
+image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=600",
+category:"Mobile Phones",
+description:"128GB | A16 Bionic | 48MP Camera"
+},
+
+{
+id:2,
+name:"Samsung Galaxy S24",
+price:74999,
+image:"https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600",
+category:"Mobile Phones",
+description:"256GB | AMOLED Display | 50MP Camera"
+},
+
+{
+id:3,
+name:"OnePlus 13",
+price:58999,
+image:"https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?w=600",
+category:"Mobile Phones",
+description:"256GB | Snapdragon Processor"
+},
+
+{
+id:4,
+name:"Redmi Note 14",
+price:18999,
+image:"https://images.unsplash.com/photo-1580910051074-3eb694886505?w=600",
+category:"Mobile Phones",
+description:"8GB RAM | 256GB Storage"
+}
+
+];
