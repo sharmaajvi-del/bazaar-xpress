0eb801a880d9bb16ceae9dac71051a7184b2ca44
sharmaajvi-del <sharmaajvi@gmail.com>
2026-06-29 07:02:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+// Bazaar Xpress App
 
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(product){
+
+cart.push(product);
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
+alert(product.name + " added to cart");
+
+}
+
+function getCart(){
+
+return JSON.parse(localStorage.getItem("cart")) || [];
+
+}
+
+function clearCart(){
+
+localStorage.removeItem("cart");
+
+}
+
+function getProduct(id){
+
+return products.find(p => p.id == id);
+
+}
