5fe637a2df261830b86af619a3701640119bc997
sharmaajvi-del <sharmaajvi@gmail.com>
2026-06-29 16:48:06 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,39 +1,43 @@
-const products = [
+import { db } from "./firebase.js";
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-{
-id:1,
-name:"iPhone 15",
-price:69999,
-image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=600",
-category:"Mobile Phones",
-description:"128GB | A16 Bionic | 48MP Camera"
-},
+const productList = document.getElementById("productList");
 
-{
-id:2,
-name:"Samsung Galaxy S24",
-price:74999,
-image:"https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?w=600",
-category:"Mobile Phones",
-description:"256GB | AMOLED Display | 50MP Camera"
-},
+async function loadProducts() {
 
-{
-id:3,
-name:"OnePlus 13",
-price:58999,
-image:"https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?w=600",
-category:"Mobile Phones",
-description:"256GB | Snapdragon Processor"
-},
+  const querySnapshot = await getDocs(collection(db, "products"));
 
-{
-id:4,
-name:"Redmi Note 14",
-price:18999,
-image:"https://images.unsplash.com/photo-1580910051074-3eb694886505?w=600",
-category:"Mobile Phones",
-description:"8GB RAM | 256GB Storage"
+  let html = "";
+
+  querySnapshot.forEach((doc) => {
+
+    const product = doc.data();
+
+    html += `
+    <div class="card">
+
+      <img src="${product.image || 'https://via.placeholder.com/300'}">
+
+      <h2>${product.name}</h2>
+
+      <p>${product.description}</p>
+
+      <div class="price">₹${product.price}</div>
+
+      <button>
+        View Product
+      </button>
+
+    </div>
+    `;
+
+  });
+
+  productList.innerHTML = html;
+
 }
 
-];
+loadProducts();
